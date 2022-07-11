4d7d02995acc66dbe7dfa47b5fcb6ac0aa613192
misho <mishikoqajaia50@gmail.com>
2022-07-11 14:48:36 +0400

    Media service base setup

--- media/src/resources/file/file.service.ts
+++ media/src/resources/file/file.service.ts
@@ -1,8 +1,54 @@
-const { ROOT_URL } = process.env;
+import ProfileImage from "@/entity/ProfileImage";
+import HttpException from "@/utils/exceptions/http.exception";
+import Media from "@/entity/Media";
+// import MediaType from "@/entity/MediaType";
+import { MediaTypes } from "@/entity/Media";
+import PostMedia from "@/entity/PostMedia";
 
+export type IUploadManyArgs = {
+  mediaURL: String;
+  mediaType: MediaTypes;
+};
 class FileService {
-  public upload = async () => {};
-  public uploadMany = async () => {};
+  public upload = async (
+    userId: number,
+    mediaURL: string,
+    mediaType: MediaTypes
+  ) => {
+    await Media.insert({
+      media_url: mediaURL,
+      user_id: userId,
+      type: mediaType,
+    });
+  };
+  public uploadMany = async (
+    userId: number,
+    uploadManyArgs: IUploadManyArgs[]
+  ) => {
+    await Promise.all(
+      uploadManyArgs.map(async (item) => {
+        await Media.insert({
+          media_url: item.mediaURL.toString(),
+          user_id: userId,
+          type: item.mediaType,
+        });
+      })
+    );
+  };
+  public uploadProfileImage = async (userId: number, imageURL: string) => {
+    await ProfileImage.insert({
+      image_url: imageURL,
+      user_id: userId,
+    });
+  };
+  public uploadPostMedia = async (userId: number, postId:string, mediaURL: string, mediaType:MediaTypes) => {
+    await PostMedia.insert({
+      media_url: mediaURL,
+      user_id: userId,
+      post_id: postId,
+      type:mediaType
+    });
+  };
 }
 
 export default FileService;
---
+++ media/src/resources/user/user.interface.ts
@@ -0,0 +1,8 @@
+export interface IUser {
+  id: number;
+  email: string;
+  username: string;
+  verified: boolean;
+  iat?: string;
+  exp?: boolean;
+}
