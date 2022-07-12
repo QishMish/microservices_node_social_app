a6813e7ee05edd30d99a418d36a17a1397b289f2
misho <mishikoqajaia50@gmail.com>
2022-07-12 21:54:07 +0400

    Remove intermediate post and comment services

--- post/src/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-function getMethods(object) {
-  return Object.getOwnPropertyNames(Object.getPrototypeOf(object)).filter(
-    (m) => "function" === typeof object[m]
-  );
-}
-
-module.exports = {
-  getMethods,
-};
